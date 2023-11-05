7975da3cf41f8c5f4bf90aac9b980d7d6ff4323f
surya1425 <surya@codegama.com>
2023-11-05 22:43:40 +0530

    chnages

--- script/main.js
+++ script/main.js
@@ -1,15 +1,20 @@
-$(document).ready(function() {
-    var $lis = $("banner ul.list-unstyled li");
+// $(document).ready(function() {
+//     var $lis = $("banner ul.list-unstyled li");
 
-    $lis.hover(function() {
-      $(this).addClass("hovered");
-    }, function() {
-      $(this).removeClass("hovered");
-    });
+//     $lis.hover(function() {
+//       $(this).addClass("hovered");
+//     }, function() {
+//       $(this).removeClass("hovered");
+//     });
 
-    $lis.mouseout(function() {
-      if (!$lis.hasClass("hovered")) {
-        $lis.css("width", "50%");
-      }
-    });
-  });
+//     $lis.mouseout(function() {
+//       if (!$lis.hasClass("hovered")) {
+//         $lis.css("width", "50%");
+//       }
+//     });
+//   });
+
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+});
